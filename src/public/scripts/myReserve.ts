2967d82a157a8fcad1e594d73c32c59afdec5339
Facundo Fernández <facuu201202@gmail.com>
2024-10-21 18:07:29 -0300

    Se termino de diseñar la seccion de reservas, solo queda agregar la funcion al boton cancelar

--- src/public/scripts/myReserve.ts
+++ src/public/scripts/myReserve.ts
@@ -30,36 +30,59 @@ const reservasCard = (reservas: any[]) =>{
         const divCard = document.createElement("div");
         divCard.classList.add("card-reserva");
 
+        const divTitle = document.createElement("div")
+        divTitle.classList.add("card-title")
         const title = document.createElement("h3");
         const fechaAdaptada = obtenerFecha(reserva.fecha)
         title.textContent = fechaAdaptada
-        title.classList.add("card-title")
+        title.classList.add("card-fecha")
+        const iconTrash = document.createElement("i")
+        iconTrash.classList.add("fa-regular", "fa-trash-can")
+
+        iconTrash.addEventListener('click', () => {
+            const confirmed = confirm(`¿Estás seguro de que deseas cancelar la reserva del ${fechaAdaptada} ?`);
+            console.log(confirmed)
+        })
+
+        divTitle.appendChild(title)
+        divTitle.appendChild(iconTrash)
 
         const cardBody = document.createElement("div")
         cardBody.classList.add("card-body")
 
 
         const divHora = document.createElement("div")
+        const iconHora = document.createElement("i")
+        iconHora.classList.add("fa-regular", "fa-clock")
         const hora = document.createElement("p")
         hora.textContent = "Hora: " +  reserva.hora_detalle
+        divHora.appendChild(iconHora)
         divHora.appendChild(hora)
         divHora.classList.add("card-item")
 
 
         const divPersonas = document.createElement("div")
+        const iconPersonas = document.createElement("i")
+        iconPersonas.classList.add("fa-regular", "fa-user")
         const personas = document.createElement("p")
         personas.textContent = "Personas: "  + reserva.cantidad
+        divPersonas.appendChild(iconPersonas)
         divPersonas.appendChild(personas)
         divPersonas.classList.add("card-item")
 
         const divMesa = document.createElement("div")
+        const iconMesa = document.createElement("i")
+        iconMesa.classList.add("fa-solid", "fa-chair")
         const mesa = document.createElement("p")
         mesa.textContent = "Mesa: " + reserva.id_mesa
+        divMesa.appendChild(iconMesa)
         divMesa.appendChild(mesa)
         divMesa.classList.add("card-item")
 
 
         const estadoContainer = document.createElement("div")
+        const iconEstado = document.createElement("i")
+        iconEstado.classList.add("fa-regular", "fa-square-check")
         estadoContainer.classList.add("estado-container")
         estadoContainer.classList.add("card-item")
 
@@ -67,13 +90,13 @@ const reservasCard = (reservas: any[]) =>{
         const estadoText = document.createElement("p")
         estadoText.textContent =  "Estado: " 
         estado.textContent = reserva.estado
-        estado.classList.add("estado")
-        
+        estado.classList.add(reserva.estado)
+        estadoContainer.appendChild(iconEstado)
         estadoContainer.appendChild(estadoText)
         estadoContainer.appendChild(estado)
      
 
-        divCard.appendChild(title)
+        divCard.appendChild(divTitle)
         cardBody.appendChild(divHora)
         cardBody.appendChild(divPersonas)
         cardBody.appendChild(divMesa)
